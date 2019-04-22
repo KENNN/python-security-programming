b5affdb735f14a24a6e83b315d81622ce97c4519
KENN <cent.rngttwt@gmail.com>
2019-04-22 11:36:31 +0900

    add tcp client

---
+++ chapter02/tcp_client.py
@@ -0,0 +1,19 @@
+# -*- /usr/bin/python
+# -*- coding: utf-8 -*-
+
+import socket
+
+if __name__ == "__main__" :
+    ip = '127.0.0.1'
+    port = 50000
+    server = (ip, port)
+    msg = ''
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect(server)
+
+        while msg != 'exit' :
+            msg = input('> ')
+            s.send(msg.encode())
+            data = s.recv(1024)
+            print('Received from server: {}'.format(str(data)))
