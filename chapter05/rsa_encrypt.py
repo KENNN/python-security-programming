385505f7a7d0f2b2924b25dbb34333575bbc1f5b
KENN <cent.rngttwt@gmail.com>
2019-06-22 21:19:13 +0900

    add the RSA encryptor draft

---
+++ chapter05/rsa_encrypt.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+def gen_public_key():
+    pass
+
+
+def encrypt():
+    pass
+
+
+def run():
+    pass
+
+
+def main():
+    run()
+
+
+if __name__ == '__main__':
+    main()
