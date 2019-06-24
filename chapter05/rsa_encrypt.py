2b4118bfe62969061f6fb415a4166ac3cc66707a
KENN <cent.rngttwt@gmail.com>
2019-06-24 23:26:27 +0900

    add the RSA encryptor

--- chapter05/rsa_encrypt.py
+++ chapter05/rsa_encrypt.py
@@ -8,8 +8,17 @@ def gen_public_key(p, q):
     return e, n
 
 
-def encrypt():
-    pass
+def encrypt(plaintext):
+    p, q = 513131, 248021
+    e, n = gen_public_key(p, q)
+    plaintext = list(plaintext)
+    cipher = []
+
+    for t in plaintext:
+        c = pow(t, e, n)
+        cipher.append(c)
+
+    return cipher
 
 
 def run():
