c76753521769fcb229e8558c49ab37d0031ec32d
KENN <cent.rngttwt@gmail.com>
2019-06-26 23:12:33 +0900

    add the RSA decryptor draft

---
+++ chapter05/rsa_decrypt.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+
+def ex_gcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        gcd, x, y = ex_gcd(b % a, a)
+        return (gcd, y - int(b / a) * a, x)
+
+
+def gen_private_key(e, phi_n):
+    pass
+
+
+def decrypt(cipher):
+    pass
+
+
+def run():
+    pass
+
+
+def main():
+    run()
+
+
+if __name__ == '__main__':
+    main()
